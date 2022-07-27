dbfa14e766d8d957d68fc1fc36942c00ce7de0d1
CatsPsychologist <mainofficee@gmail.com>
2022-07-27 12:26:02 +0300

    in

--- Portfolio/js/app.js
+++ Portfolio/js/app.js
@@ -5,3 +5,37 @@ let email = document.getElementById('email_copy')
 email.onclick = () => {
     navigator.clipboard.writeText('artem.cherednychenko.k@gmail.com')
 }
+
+
+// header feature that attaches header to the page after making a scroll that equals to headers height
+// and returning it back if you get to the top of the page
+const header = document.querySelector('.header')
+
+console.log(header.clientHeight)
+
+let lastScroll = 0;
+const defaultOffset = header.clientHeight;
+
+
+const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
+const containHide = () => header.classList.contains('hide');
+
+window.addEventListener('scroll', () => {
+    if(scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset) {
+        //scroll down
+        header.classList.add('hide');
+    }
+
+
+    // else if(scrollPosition() < lastScroll && containHide()){
+    //     //scroll up
+    //     header.classList.remove('hide');
+    // }
+    if(scrollPosition() === 0){
+        header.classList.remove('hide');
+    }
+    // console.log(lastScroll)
+    // console.log(defaultOffset)
+    lastScroll = scrollPosition();
+    // console.log(scrollPosition())
+})
