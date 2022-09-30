9cd9117519542481c1895ca451c714b52d309f63
CatsPsychologist <mainofficee@gmail.com>
2022-09-30 19:21:43 +0300

    upd

--- hys_academy/ls_1/js/mobile-menu.js
+++ hys_academy/ls_1/js/mobile-menu.js
@@ -1,3 +1,3 @@
 // actually the whole menu was written without js, but it has to be closed as soon as user click to navigation
-const header_nav = document.getElementById('header_nav')
-header_nav.addEventListener('click', () => {document.getElementById('menu-toggle').checked = false});
+const headerNav = document.getElementById('header_list');
+headerNav.addEventListener('click', () => {document.getElementById('menu-toggle').checked = false});
