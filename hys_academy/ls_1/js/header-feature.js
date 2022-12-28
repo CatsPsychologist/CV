877e0a7a6c6c63a9748070a2884a8e82bd71d864
CatsPsychologist <mainofficee@gmail.com>
2022-12-28 08:28:44 +0200

    added my hys project to cv

--- hys_academy/ls_1/js/header-feature.js
+++
@@ -1,5 +0,0 @@
-const header = document.getElementById("header")
-window.addEventListener('scroll', function() {
-    if (window.scrollY >= header.clientHeight) { header.classList.add("header_primary") }
-    else { header.classList.remove("header_primary")}
-})
--- hys_academy/ls_1/js/mobile-menu.js
+++
@@ -1,3 +0,0 @@
-// actually the whole menu was written without js, but it has to be closed as soon as user click to navigation
-const headerNav = document.getElementById('header_list');
-headerNav.addEventListener('click', () => {document.getElementById('menu-toggle').checked = false});
