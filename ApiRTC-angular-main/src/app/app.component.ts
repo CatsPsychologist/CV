4c1ac5d4b079874da4858372f033e828ba393cac
Artem Cherednychenko <83455895+CatsPsychologist@users.noreply.github.com>
2024-04-18 13:29:47 +0300

    upd

---
+++ ApiRTC-angular-main/src/app/app.component.ts
@@ -0,0 +1,129 @@
+import { Component, ViewChild, ElementRef } from '@angular/core'
+import { FormBuilder, FormControl, Validators } from '@angular/forms'
+
+import { Conversation, UserAgent, Session, Stream } from '@apirtc/apirtc'
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title = 'ApiRTC-angular';
+
+  @ViewChild("localVideo") videoRef: ElementRef;
+
+  conversationFormGroup = this.fb.group({
+    name: this.fb.control('', [Validators.required])
+  });
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  get conversationNameFc(): FormControl {
+    return this.conversationFormGroup.get('name') as FormControl;
+  }
+
+  conversation: any;
+  remotesCounter = 0;
+
+  getOrcreateConversation() {
+    console.log('fist')
+    var localStream = null;
+
+    //==============================
+    // 1/ CREATE USER AGENT
+    //==============================
+    var userAgent = new UserAgent({
+      uri: 'apiKey:d710c86b379c58d1a7d73bcaab235809'
+    });
+
+    //==============================
+    // 2/ REGISTER
+    //==============================
+    userAgent.register().then((session: Session) => {
+      console.log(Session)
+      
+      console.log('register')
+    
+
+      //==============================
+      // 3/ CREATE CONVERSATION
+      //==============================
+      const conversation: Conversation = session.getConversation(this.conversationNameFc.value);
+      this.conversation = conversation;
+
+      //==========================================================
+      // 4/ ADD EVENT LISTENER : WHEN NEW STREAM IS AVAILABLE IN CONVERSATION
+      //==========================================================
+      conversation.on('streamListChanged', (streamInfo: any) => {
+        console.log('streamlist')
+        console.log("streamListChanged :", streamInfo);
+        if (streamInfo.listEventType === 'added') {
+          if (streamInfo.isRemote === true) {
+            conversation.subscribeToMedia(streamInfo.streamId)
+              .then((stream: Stream) => {
+                console.log('subscribeToMedia success', stream);
+              }).catch((err) => {
+                console.log('test')
+                console.error('subscribeToMedia error', err);
+              });
+          }
+        }
+      });
+      //=====================================================
+      // 4 BIS/ ADD EVENT LISTENER : WHEN STREAM IS ADDED/REMOVED TO/FROM THE CONVERSATION
+      //=====================================================
+      conversation.on('streamAdded', (stream: Stream) => {
+        console.log('stream added')
+        this.remotesCounter += 1;
+        stream.addInDiv('remote-container', 'remote-media-' + stream.streamId, {}, false);
+      }).on('streamRemoved', (stream: any) => {
+        this.remotesCounter -= 1;
+        stream.removeFromDiv('remote-container', 'remote-media-' + stream.streamId);
+      });
+
+      //==============================
+      // 5/ CREATE LOCAL STREAM
+      //==============================
+      userAgent.createStream({
+        constraints: {
+          audio: true,
+          video: true
+        }
+      })
+        .then((stream: Stream) => {
+
+          console.log('createStream :', stream);
+
+          // Save local stream
+          localStream = stream;
+
+          // Display stream
+          localStream.attachToElement(this.videoRef.nativeElement);
+
+          //==============================
+          // 6/ JOIN CONVERSATION
+          //==============================
+          conversation.join()
+            .then(() => {
+              //==============================
+              // 7/ PUBLISH LOCAL STREAM
+              //==============================
+              conversation.publish(localStream).then((stream: Stream) => {
+                console.log('published', stream);
+              }).catch((err: any) => {
+                console.error('publish error', err);
+              });
+            }).catch((err: any) => {
+              console.error('Conversation join error', err);
+            });
+        }).catch((err: any) => {
+          console.error('create stream error', err);
+        });
+    });
+  }
+}
